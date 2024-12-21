4077ccc3df180f6af0f55b466028bbeaa02ff254
MahmoudRizaee <mahmoudnaeebi2006@gmail.com>
2024-12-21 09:56:57 +0430

    fff
    gg

--- Mahmoud/CSSpl/Test16.js
+++ Mahmoud/CSSpl/Test16.js
@@ -1,1 +1,19 @@
+function dark() {
+  document.getElementById("body").classList.add('dark');
+}
+function light() {
+  document.getElementById("body").classList.remove('dark');
+}
 
+
+
+
+function popup(){
+
+  document.getElementById("pop").classList.add("showwindow")
+}
+
+function popup_close(){
+
+  document.getElementById("pop").classList.remove("showwindow")
+}
