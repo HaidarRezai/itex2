1be882d155fc2040f2b4a6689c8b6727d98b6e53
SalamKhan10 <salamkhanaliyar@gmail.com>
2025-01-07 21:19:07 -0800

    table

---
+++ SalamKhan/Java script/index6.js
@@ -0,0 +1,8 @@
+  
+let images =['../game.gpg.jpg','../ball.gpg.jpg','/fish.gpg.jpg,']
+let counter = 0
+  
+let x = `<img src="${images[counter]}" width="200px" alt=""></img>`
+
+
+document.getElementById("head").innerHTM
---
+++ SalamKhan/Java script/text.js
@@ -0,0 +1,7 @@
+function sum(){
+    let x1 = Number(document.getElementById("num1").value)
+    let x2 = Number(document.getElementById("num2").value)
+    let x3 = Number(document.getElementById("num3").value)
+    let x4 = Number(document.getElementById("num4").value)
+    document.getElementById("inne").innerHTML = x1 + x2 + x3 + x4 /4
+}
